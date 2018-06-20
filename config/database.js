613101d46e0820551865f93820dd7e8c6553b6e2
ZamurVedovatto <vedovatto@gmail.com>
2018-06-20 19:14:16 -0300

    Finalizado

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
-/*
 module.exports = {
     database: 'mongodb://zamur:123@ds243345.mlab.com:43345/restful-api',
     secret: '123'
 }
-*/
 
+/*
 module.exports = {
     database: 'mongodb://localhost:27017/sisloc',
     secret: '123'
 };
+*/
